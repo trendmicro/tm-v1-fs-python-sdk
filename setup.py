4f167e4985ad1d955b30fb56b76f621234e4490f
yusongz <Yusong_Zhang@trendmicro.com>
2024-04-03 20:31:29 -0700

    test

--- setup.py
+++ setup.py
@@ -2,23 +2,28 @@ from setuptools import setup, find_namespace_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
-with open("public/VERSION", "r") as fh:
-    _version = fh.read().strip()
+with open("VERSION", "r") as fh:
+    package_version = fh.read().strip()
 setup(
-    name="amaas-grpc",
-    version=_version,
-    author="Trend Micro Team San Jose",
-    description="Project AMaaS >_",
+    name="visionone-filesecurity",
+    version=package_version,
+    author="Trend Micro VisionOne File Security Team",
+    description="Trend Micro VisionOne File Security SDK for python",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://www.trendmicro.com/",
-    packages=find_namespace_packages(where="public", exclude=["tests*", "examples"]),
-    package_dir={"": "public"},
+    url="https://github.com/trendmicro/tm-v1-fs-python-sdk",
+    packages=find_namespace_packages(exclude=["tests*", "examples"]),
+    package_data={"amaas": ["grpc/protos/*"]},
     include_package_data=True,
     classifiers=[
+        "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "License :: Other/Proprietary License",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "License :: OSI Approved :: MIT License",
     ],
-    python_requires=">=3.9",
+    python_requires=">=3.7",
 )
-
